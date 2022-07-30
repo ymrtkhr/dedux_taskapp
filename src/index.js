4211ab210f92006ba601bedfc1c93c0d820d4ffd
yamori <yamori@riso.co.jp>
2022-07-30 23:03:13 +0900

    all done

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
-const container = document.getElementById('root');
+import Login from "./features/login/Login";
+import { Route, BrowserRouter } from "react-router-dom";
+
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/tasks" component={App} />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
